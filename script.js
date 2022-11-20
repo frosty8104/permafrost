f5e94ac2b84d128789b84b3aa804f2a062b6ecd8
laura <lg8104@gmail.com>
2022-11-19 17:23:33 -0700

    Creating remote portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector("button");
+
+button.addEventListener ("click", function () {
+    div.classList.add(show);
+    button.innerText.remove;
+});
+const div = document.createElement("div");
+div.innerHTML = 
+`<h2>Project Title: </h2>
+<p>Type/Scope: </p>
+<p>Role: </p>
+<p>Overview: </p>
+<p>Goals & Problems Solved: </p>
+<p>Design Process: </p>
+<p>Challenges & Takeaways: </p>
+<p>Conclusion: </p>`;
