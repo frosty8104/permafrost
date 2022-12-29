d4acf0c6e96340bda59083ce9052565b6899a89e
laura <lg8104@gmail.com>
2022-12-29 13:13:23 -0700

    Updating pages

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ div.innerHTML =
 <p>Design Process: </p>
 <p>Challenges & Takeaways: </p>
 <p>Conclusion: </p>`;
+
+const mobile = document.querySelector(".burger");
+const list = document.querySelector("ul");
+mobile.addEventListener("click", mobileMenu);
+function mobileMenu() {
+    mobile.classList.toggle("active");
+    list.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll("a");
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+function closeMenu() {
+    burger.classList.remove("active");
+    list.classList.remove("active");
+}
