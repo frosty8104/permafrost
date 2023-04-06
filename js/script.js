977610ef111619e6b97e3b2ab7bf92926900264d
Laura Gieg <lg8104@gmail.com>
2023-04-06 09:22:24 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
-/* display needs: 2 buttons (Case Study, Portfolio; extract text from pdf: fetch & display text in created div)
-const button = document.querySelector("button");
-
-//Fetch and display case study text
+/* display needs: 2 buttons (Case Study, Portfolio; extract text from pdf: fetch & display text in created div) */
+/*const button = document.querySelector("button");
 //Add button to display case study text
-//Edit button to hide case study div
-
 button.addEventListener ("click", function () {
     div.classList.add(show);
     button.innerText.remove;
 });
+//Edit button to hide case study div
+
+//Fetch and display case study text
+const div = document.createElement("div");
+div.innerHTML = 
+`<h2>Project Title: </h2>
+<p>Type/Scope: </p>
+<p>Role: </p>
+<p>Overview: </p>
+<p>Goals & Problems Solved: </p>
+<p>Design Process: </p>
+<p>Challenges & Takeaways: </p>
+<p>Conclusion: </p>`;
+const div = document.createElement("div");
+div.innerHTML = 
+`<h2>Project Title: </h2>
+<p>Type/Scope: </p>
+<p>Role: </p>
+<p>Overview: </p>
+<p>Goals & Problems Solved: </p>
+<p>Design Process: </p>
+<p>Challenges & Takeaways: </p>
+<p>Conclusion: </p>`;
 const div = document.createElement("div");
 div.innerHTML = 
 `<h2>Project Title: </h2>
@@ -19,10 +38,9 @@ div.innerHTML =
 <p>Design Process: </p>
 <p>Challenges & Takeaways: </p>
 <p>Conclusion: </p>`;*/
-
+// Burger menu
 const burger = document.querySelector(".burger");
 const links = document.querySelector(".links");
-
 burger.addEventListener('click', () => {
     links.classList.toggle('active')
-})
+});
