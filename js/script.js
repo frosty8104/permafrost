c0486ecc4cc2283fc53e07aeb8585a482bbfa1e6
laura <lg8104@gmail.com>
2023-04-06 09:55:53 -0600

    Updating pages

--- js/script.js
+++ js/script.js
@@ -43,4 +43,4 @@ const burger = document.querySelector(".burger");
 const links = document.querySelector(".links");
 burger.addEventListener('click', () => {
     links.classList.toggle('active')
-})
+});
