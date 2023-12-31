4abcb1dd77f8c65f86a54da2dfbe1c8050fa37d7
laura <lg8104@gmail.com>
2023-12-30 18:45:32 -0700

    Updating stylesheet, javascript file, and Whole Bean case study page

--- js/script.js
+++ js/script.js
@@ -11,3 +11,14 @@ date.innerHTML = year;
 // Case study image modal
 const img = document.querySelector("img");
 const modal = document.querySelector(".modal");
+const modalImg = document.querySelector("#page");
+const captionText = document.querySelector("#caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText = this.alt;
+}
+const span = document.querySelector(".close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
